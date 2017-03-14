fe691863b1ccc1d6beb2c6641b87fbcd406d5179
dannych <dnny.ch@gmail.com>
2017-03-14 21:13:45 +0700

    fix extension

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -137,7 +137,7 @@ module.exports = {
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: ['style-loader','css-loader','sass-loader', {
           loader: 'postcss-loader',
           options: {
